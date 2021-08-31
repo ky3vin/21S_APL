f51b3b73d5262ae80679de3f634ee903aae7837e
yevinKwon <kimpika8541@naver.com>
2021-09-01 03:44:16 +0900

    Update prefixSpan_edit.py

--- PrefixSpan/prefixSpan_edit.py
+++ PrefixSpan/prefixSpan_edit.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# 결과 예시 :<br/>
-# (['210.123.38.21', '80', 'TCP', '국민대학교'...],['웹해킹'],99) <br/>
-# 1. 첫 번째 []는 `INST_NM`, `ASSETS_VAL_1-22`, `TW_ATT_IP`,`TW_ATT_PORT`,`TW_DMG_IP`,`TW_DMG_PORT`,`TW_ATT_CT_CODE`, `INTENT_VAL_0-6` <br/>
-# 2. 두 번째 []는 `DRULE_ATT_TYPE_CODE1`
-# 3. 빈도수
-
-# In[1]:
-
-
 # 원하는 파일 불러오기
 import pandas as pd
 df = pd.read_csv("./data/ts_data_accident-2020_sample.csv")
 df.head()
-
-
-# In[2]:
-
 
 #필요한 라이브러리 불러오기
 import numpy as np
@@ -29,37 +16,6 @@ import time
 
 # ### 데이터 추출하기 : INST_NM, ASSETS_VAL, INTENT_VAL
 
-# In[3]:
-
-
-'''
-#RISK_V2에서 ASSETS_VAL 추출
-df_ASSET = df.loc[:,['RISK_V2']] #전체 dataframe에서 'RISK_V2'col만 추출하여 만든 dataframe
-df_temp = pd.DataFrame() #df_risk의 i번째 row의 값을 임시로 저장할 df
-
-start = time.time()  # 시작 시간 저장
-
-for i in range(len(df)): #len(df) : 26043
-    js = df_ASSET['RISK_V2'][i] #type(js) : str
-    js = js.replace("'", "\"") # 홑따옴표->겹따옴표(JSON 표준)
-    js_string = json.loads(js) #JSON 문자열을 Python 객체로 변환
-    json_df = json_normalize(js_string) #JSON 객체를json_normalize()에 전달하면 필요한 데이터가 포함 된 Pandas DataFrame이 반환
-    df_temp = pd.concat([df_temp,json_df],ignore_index=True) 
-    #방금 구한 json_df를 기존df에 추가(json_df의 index는 모두 0이므로 ignore_index로 새롭게 index 설정 필요)
-    
-print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
-
-# 시간이 오래 걸려서 매번 실행할 때 마다 처리하는건 불편. 따로 파일 만들어 저장
-
-ASSETS_VAL= df_temp.copy()[['ASSETS_VAL_1','ASSETS_VAL_2','ASSETS_VAL_3','ASSETS_VAL_4','ASSETS_VAL_5'
-                            ,'ASSETS_VAL_6','ASSETS_VAL_7','ASSETS_VAL_8','ASSETS_VAL_9','ASSETS_VAL_10','ASSETS_VAL_11',
-                            'ASSETS_VAL_12','ASSETS_VAL_13','ASSETS_VAL_14','ASSETS_VAL_15','ASSETS_VAL_16','ASSETS_VAL_17',
-                            'ASSETS_VAL_18','ASSETS_VAL_19','ASSETS_VAL_20','ASSETS_VAL_21','ASSETS_VAL_22']]
-ASSETS_VAL.to_csv("./data/ts_data_accident-2020_sample_ASSETS_VAL.csv")
-
-# 마찬가지로 INTENT_VAL 과 INST_NM 추출 및 저장
-'''
-print() #주석 markdown 출력 방지용
 
 
 # ### 필요한 데이터로 데이터프레임 만들기 : TW_ATT_IP, TW_ATT_PORT, TW_DMG_PORT, TW_DMG_IP, DRULE_ATT_TYPE_CODE1, TW_ATT_CT_CODE, INTENT_VAL, ASSETS_VAL, INST_NM
